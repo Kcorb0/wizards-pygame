94e4564a373121b46e8930df1358f79e67abb333
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-15 15:07:20 +0100

    Pyromancer and Necromancer classes created

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ pygame.init()
 
 # 1280 x 720 display
 WIDTH, HEIGHT = 1280, 720
-
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Wizards")
+bg = pygame.image.load("assets/spooky_bg.jpg")
 
 
-run = True
-
-while run:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    pygame.display
+    window.blit(bg, [0, 0])
+    pygame.display.update()
--- wizards.py
+++ wizards.py
@@ -1,6 +1,48 @@
+import pygame
+
+
 class Wizard:
-    def __init__(self, name, age, health=10, pwr_level=100):
+    def __init__(self, name, age, speed=50, health=30, mana=3):
         self.name = name
         self.health = health
         self.age = age
-        self.pwr_level = pwr_level
+        self.mana = mana
+
+    def increase_mana(self):
+        increment = 1
+        self.mana += increment
+
+    def use_spell(self, spell):
+        # Method takes a list as arguments ['Cast Doubt', 4, 5, other effects]
+        title = spell[0]
+        dmg = spell[1]
+        mana = spell[2]
+
+        print(
+            f"{self.name} uses {title} dealing ({dmg}) damage. Mana reduced by ({mana})."
+        )
+
+    def open_spellbook(self):
+        pass
+
+
+class Pyromancer(Wizard):
+    def __init__(self, name, age, speed=55, health=30, mana=3):
+        super().__init__(name, age, health=health, mana=mana)
+        self.spells = {}  # Dict of wizard spells {title: [dmg, mana, other effects]}
+        self.sprite = None
+
+
+class Necromancer(Wizard):
+    def __init__(self, name, age, speed=40, health=30, mana=3):
+        super().__init__(name, age, health=health, mana=mana)
+        self.spells = []  # List of wizard spells [title, dmg, mana, other effects]
+        self.sprite = None
+
+
+if __name__ == "__main__":
+    test_wiz = Pyromancer("Clint Eastwood", 25)
+
+    print(test_wiz.mana)
+    test_wiz.increase_mana()
+    print(test_wiz.mana)
