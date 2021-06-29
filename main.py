f7ea19a31483b0bb8d6b41cf648ca0ecfa876b01
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-29 01:05:26 +0100

    Created a bunch of new sprites, refactored a lot of code. Spells are now randomly chosen and both sides are given their three spells to use.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from pygame import font
-from pygame.display import update
-from wizards import Pyromancer, Necromancer
 import pygame
 import sys
 import random as rand
+from wizards import Pyromancer, Necromancer
+
+# Notes
+# Original BG size is (270x152) scaled to (1280x720)
+# Sprites and images are scaled by x4.74 from original size
+# Change health bar to hears, hearts can be halfed, players start with a row of armored hears which need to be destroyed first
+# Armored hearts cannot be healed once destroyed. May add some mechanics for this later.
 
 pygame.init()
 pygame.font.init()
@@ -12,26 +16,23 @@ pygame.font.init()
 WIDTH, HEIGHT = 1280, 720
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Wizards")
-bg = pygame.image.load("assets/new_spooky_bg.png")
+bg = pygame.image.load("assets/backgrounds/bg_corridor.jpg")
 font_casc_standard = pygame.font.SysFont("Cascadia Mono", 50)
 font_casc_small = pygame.font.SysFont("Cascadia Mono", 35)
 
 
 def mana_orbs(p1_mana, p2_mana):
 
-    mana_orb = pygame.image.load("assets/mana_orb.png")
-    mana_orb_scaled = pygame.transform.scale(
-        mana_orb, [int(mana_orb.get_width() * 0.14), int(mana_orb.get_height() * 0.14)]
-    )
+    mana_orb = pygame.image.load("assets/health_and_mana/mana_orb.png")
 
     p1_x = 25
     for i in range(0, p1_mana):
-        window.blit(mana_orb_scaled, [p1_x, 100])
+        window.blit(mana_orb, [p1_x, 100])
         p1_x += 45
 
     p2_x = 60
     for x in range(0, p2_mana):
-        window.blit(mana_orb_scaled, [WIDTH - p2_x, 100])
+        window.blit(mana_orb, [WIDTH - p2_x, 100])
         p2_x += 45
 
 
@@ -54,60 +55,48 @@ def health_bar(p1_hp, p2_hp):
         window, (65, 179, 57), pygame.Rect(WIDTH - 425, 70, bar2_width, 20)
     )
 
+def blit_spell(spell, img_pos, dmg_pos, mana_pos):
 
-def spell_board():
-    spell_board = pygame.image.load("assets/spell_board.png")
-    spell_board_scaled = pygame.transform.scale(
-        spell_board,
-        [
-            int(spell_board.get_width() * 0.7),
-            int(spell_board.get_height() * 0.7),
-        ],
-    )
-    window.blit(spell_board_scaled, [25, 575])
-    window.blit(spell_board_scaled, [WIDTH - spell_board_scaled.get_width() - 25, 575])
+    # Blit spell img
+    window.blit(spell[3], img_pos)
+
+    dmg_txt = font_casc_small.render(f"{spell[1]}", True, (255, 255, 255))
+    window.blit(dmg_txt, dmg_pos)
+
+    mana_txt = font_casc_small.render(f"{spell[2]}", True, (0, 0, 0))
+    window.blit(mana_txt, mana_pos)
+
+def generate_spell(player, pos):
+    # Spell click range between spell width with mouse pos
+    
+    
 
 
-def generate_spells(player, pos):
+    spell1 = rand.choice(player.spellbook)
+    spell2 = rand.choice(player.spellbook)
+    spell3 = rand.choice(player.spellbook)
 
-    spell1 = player.spellbook[0]
-    spell2 = player.spellbook[1]
-    spell2 = player.spellbook[2]
+    s_dist = 190
+    s_ht = 575
 
-    # First spell
-    window.blit(spell1[3], [35, 570])
-    dmg_txt = font_casc_small.render(f"{spell1[1]}", True, (255, 194, 222))
-    window.blit(dmg_txt, [50, 650])
-    mana_txt = font_casc_small.render(f"{spell1[2]}", True, (0, 98, 204))
-    window.blit(mana_txt, [190, 580])
+    if pos == "left":
+        blit_spell(spell1, [37, s_ht], [50, 650], [205, 583])
+        blit_spell(spell2, [37+s_dist, s_ht], [50+s_dist, 650], [205+s_dist, 583])
+        blit_spell(spell3, [37+(s_dist*2), s_ht], [50+(s_dist*2), 650], [205+(s_dist*2), 583])
+
+    elif pos == "right":
+        blit_spell(spell1, [670, s_ht], [683, 650], [838, 583])
+        blit_spell(spell2, [670+s_dist, s_ht], [683+s_dist, 650], [838+s_dist, 583])
+        blit_spell(spell3, [670+(s_dist*2), s_ht], [683+(s_dist*2), 650], [838+(s_dist*2), 583])
 
 
 def players(player1, player2):
 
-    scale_multiplier = 0.30
-
+    # Create player images
     player1_sprite = pygame.image.load(player1.sprite)
-    player1_sprite_scaled = pygame.transform.scale(
-        player1_sprite,
-        [
-            int(player1_sprite.get_width() * scale_multiplier),
-            int(player1_sprite.get_height() * scale_multiplier),
-        ],
-    )
+    player2_sprite = pygame.transform.flip(pygame.image.load(player2.sprite), True, False)
 
-    player2_sprite = pygame.image.load(player2.sprite)
-    player2_sprite_scaled = pygame.transform.flip(
-        pygame.transform.scale(
-            player2_sprite,
-            [
-                int(player2_sprite.get_width() * scale_multiplier),
-                int(player2_sprite.get_height() * scale_multiplier),
-            ],
-        ),
-        True,
-        False,
-    )
-
+    # Create player names text
     player1_name = font_casc_standard.render(
         f"{player1.name} {player1.title}", True, (255, 255, 255)
     )
@@ -120,46 +109,60 @@ def players(player1, player2):
     window.blit(player2_name, [WIDTH - player2_name.get_width() - 25, 25])
 
     # Blit player sprites
-    window.blit(player1_sprite_scaled, [250, 300])
-    window.blit(
-        player2_sprite_scaled, [WIDTH - player2_sprite_scaled.get_width() - 250, 300]
-    )
+    window.blit(player1_sprite, [250, 250])
+    window.blit(player2_sprite, [WIDTH - player2_sprite.get_width() - 250, 250])
 
 
 def draw_objects(player1, player2):
-    # Draw Health bars (two rectangles should do)
-    # Draw mana orbs
+
     window.blit(bg, [0, 0])
     players(player1, player2)
+    # Create player health bar and mana orbs
     health_bar(player1.health, player2.health)
     mana_orbs(player1.mana, player2.mana)
-    spell_board()
-    generate_spells(player1, "left")
+    # Create generated player spells
+    generate_spell(player1, "left")
+    generate_spell(player2, "right")
 
 
 def update_turn(turn):
     turn_text = font_casc_standard.render(f"Turn {turn}", True, (255, 255, 255))
-    window.blit(turn_text, [(WIDTH / 2) - turn_text.get_width() + 25, 25])
+    window.blit(turn_text, [(WIDTH / 2) - turn_text.get_width()+50, 25])
 
 
+def lightshow(p1, p2):
+    # Why not
+    if p1.mana > 10:
+        p1.mana = 1
+        p2.mana = 1
+
+    if p1.health <= 0:
+        p1.health += 1
+        p2.health += 1
+
+    player.mana += 1
+    p2.mana += 1
+    p1.health -= .01
+    enemy.health -= .01
+
+# Necromancer("Billy", 45)
+# Pyromancer("Kcorb", 22)
 player = Pyromancer("Kcorb", 22)
-enemy = Necromancer("Billy", 45)
+enemy = Necromancer("Cerb", 45)
 player_turn = True
 turn = 1
 
 while True:
     mouse_pos = pygame.mouse.get_pos()
+    print(mouse_pos)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-        if player_turn == True:
-            turn += 1
-            if True:
-                pass
     
-    player.health -= .01
+    #lightshow(player, enemy)
+        
     draw_objects(player, enemy)
     update_turn(turn)
     pygame.display.update()
--- wizards.py
+++ wizards.py
@@ -20,12 +20,18 @@ class Wizard:
     def open_spellbook(self):
         pass
 
+    def idle_stance(self):
+        pass
+
+    def battle_stance(self):
+        pass
+
 
 class Pyromancer(Wizard):
     def __init__(self, name, age, speed=55, health=1, mana=3):
         super().__init__(name, age, speed=speed, health=health, mana=mana)
         self.title = "(Pyromancer)"
-        self.sprite = "assets/pyromancer.png"
+        self.sprite = "assets/wizards/pyromancer/pyromancer_idle.png"
         self.spellbook = [
             self.spell_explosion(),
             self.spell_firewhirl(),
@@ -37,18 +43,15 @@ class Pyromancer(Wizard):
         dmg = 8
         cost = 7
         card = pygame.image.load("assets/spells/spell_explosion.png")
-        card_scaled = pygame.transform.scale(
-            card, [int(card.get_width() * 0.45), int(card.get_height() * 0.45)]
-        )
         description = "Massive damage, destroys surroundings and lowers oponents speed."
 
-        return [title, dmg, cost, card_scaled, description]
+        return [title, dmg, cost, card, description]
 
     def spell_firewhirl(self):
         title = "Fire Whirl"
         dmg = 3
         cost = 6
-        card = pygame.image.load("assets/spells/placeholder.png")
+        card = pygame.image.load("assets/spells/spell_placeholder.png")
         description = "Flames engulf you and linger. (Take 2 damage for 2 turns)"
 
         return [title, dmg, cost, card, description]
@@ -57,7 +60,7 @@ class Pyromancer(Wizard):
         title = "Summon Spark"
         dmg = 1
         cost = 3
-        card = pygame.image.load("assets/spells/placeholder.png")
+        card = pygame.image.load("assets/spells/spell_placeholder.png")
         description = "A sentient spark joins you in combat, dealing 1 damage per turn."
 
         return [title, dmg, cost, card, description]
@@ -67,20 +70,36 @@ class Necromancer(Wizard):
     def __init__(self, name, age, speed=40, health=1, mana=3):
         super().__init__(name, age, speed=speed, health=health, mana=mana)
         self.title = "(Necromancer)"
-        self.sprite = "assets/necromancer.png"
-        self.spellbook = ["Corrupt"]
+        self.sprite = "assets/wizards/necromancer/necromancer_idle.png"
+        self.spellbook = [
+            self.spell_corrupt(), 
+            self.spell_summon_dead(), 
+            self.spell_plague()
+        ]
 
     def spell_corrupt(self):
         title = "Corrupt"
-        dmg = 8
+        dmg = 1
+        cost = 5
+        card = pygame.image.load("assets/spells/spell_placeholder.png")
+        description = "Player misses their next 2 turns, thier mind is broken."
+
+        return [title, dmg, cost, card, description]
+    
+    def spell_summon_dead(self):
+        title = "Summon Dead"
+        dmg = 2
+        cost = 4
+        card = pygame.image.load("assets/spells/spell_placeholder.png")
+        description = "Summon a dead corpse from surroundings, this guy could be thousands of years old. Deals 2 damage for 4 turns."
+
+        return [title, dmg, cost, card, description]
+    
+    def spell_plague(self):
+        title = "Plague"
+        dmg = 4
         cost = 7
-        description = "Massive damage, destroys surroundings and lowers oponents speed."
+        card = pygame.image.load("assets/spells/spell_placeholder.png")
+        description = "A strong virus that devours organs when inhailed. Deals 4 damage for 2 turns."
 
-        return [title, dmg, cost, description]
-
-
-if __name__ == "__main__":
-    os.system("cls")
-    test_wiz = Pyromancer("Clint Eastwood", 25)
-    print(test_wiz.name)
-    print(test_wiz.spellbook[0][1])
+        return [title, dmg, cost, card, description]
